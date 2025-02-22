7a45da59b484d05999547f271a655d4a0579a1e3
Kay Knöpfle <kay.knoepfle@exxeta.com>
2025-02-22 13:42:01 +0100

    220225

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -3,17 +3,15 @@ resource "azurerm_kubernetes_cluster" "aks-ba-kay" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "aksbakay"
-  node_os_channel_upgrade = "None"
   identity {
     type = "SystemAssigned"
   }
   default_node_pool {
     name            = "default"
     vm_size = "Standard_D4as_v5"
-    os_disk_type = "Ephemeral"
+    #vm_size = "Standard_D2as_v5"
     os_disk_size_gb = 50
     node_count = 1
-    enable_auto_scaling = false
     node_labels = {
         "nodepool" = "default"
     }
@@ -25,16 +23,14 @@ resource "azurerm_kubernetes_cluster" "aks-ba-kay" {
 
 resource "azurerm_kubernetes_cluster_node_pool" "perf-ba-kay" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-ba-kay.id
-  name                  = "performancenodepool"
+  name                  = "perf"
   vm_size               = "Standard_D8ads_v5"
+  #vm_size = "Standard_D2as_v5"
   node_count = 4
-  enable_auto_scaling = false
-  kubelet_disk_type = "temporary"
+  #node_count = 1
+  kubelet_disk_type = "Temporary"
   node_labels = {
         "nodepool" = "performancenodepool"
   }
-  os_disk_type = "Ephemeral"
   os_disk_size_gb = 50
-  priority = "Spot"
-  spot_max_price = -1
 }
